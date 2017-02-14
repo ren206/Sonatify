96671ef5d3a4ef7b23376bd87b1a1ef9238690fe
ren206 <michael.ren73@gmail.com>
2017-02-14 14:34:36 -0500

    Finish up backend auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,20 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      log_in!(@user)
+      render 'api/users/show'
+    else
+      render json: ['Invalid credentials'], status: 401
+    end
+  end
+
+  def destroy
+    if current_user
+      log_out!
+      render json: {}
+    else
+      render json: ['Sign in before you sign out'], status: 404
+    end
+  end
 end
