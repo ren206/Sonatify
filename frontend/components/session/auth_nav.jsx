786276e07c1cf5a3af173643a52d13c5bea6c687
ren206 <michael.ren73@gmail.com>
2017-02-15 15:07:31 -0500

    Add guest login

--- frontend/components/session/auth_nav.jsx
+++ frontend/components/session/auth_nav.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { login } from '../../actions/session_actions';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
 
 class AuthNav extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleGuest = this.handleGuest.bind(this);
+  }
 
+  handleGuest() {
+    const guestUser = {
+      username: 'guest',
+      password: 'sonatify'
+    }
+
+    this.props.login(guestUser).then(() => this.props.router.push('/browse'));
   }
 
   render() {
@@ -14,7 +26,11 @@ class AuthNav extends React.Component {
         <button onClick={this.props.updateFormType("signup")}>Sign Up</button>
 
         <br/>
-        
+
+        <button onClick={this.handleGuest}>Login as Guest</button>
+
+        <br/>
+
         <a href="#" onClick={this.props.updateFormType("login")}>
           Already have an account? Log in here.
         </a>
@@ -23,4 +39,20 @@ class AuthNav extends React.Component {
   }
 }
 
-export default AuthNav;
+const mapStateToProps = ({ session }, { location }) => {
+  return {
+    currentUser: session.currentUser,
+    errors: session.errors
+  }
+}
+
+const mapDispatchToProps = (dispatch, { location }) => {
+  return {
+    login: user => dispatch(login(user))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(AuthNav));
