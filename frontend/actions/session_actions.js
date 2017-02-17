1a4e85012e1e7991c20c020da02463cf0187e943
Michael Ren <michael.ren73@gmail.com>
2017-02-17 00:57:38 -0500

    Create and handle clear errors action

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,24 +2,26 @@ import * as APIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
 export const login = user => dispatch => {
-  return APIUtil.login(user)
-    .then(user => dispatch(receiveCurrentUser(user)),
-          err => {
-            return dispatch(receiveErrors(err.responseJSON));
-          });
+  return APIUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  );
 }
 
 export const logout = () => dispatch => {
-  return APIUtil.logout()
-    .then( () => dispatch(receiveCurrentUser(null)));
+  return APIUtil.logout().then(
+    () => dispatch(receiveCurrentUser(null))
+  );
 }
 
 export const signup = user => dispatch => {
-  return APIUtil.signup(user)
-    .then(user => dispatch(receiveCurrentUser(user)),
-          err => dispatch(receiveErrors(err.responseJSON)));
+  return APIUtil.signup(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    err => dispatch(receiveErrors(err.responseJSON))
+  );
 }
 
 export const receiveCurrentUser = currentUser => {
@@ -33,5 +35,11 @@ export const receiveErrors = errors => {
   return {
     type: RECEIVE_ERRORS,
     errors
+  }
+}
+
+export const clearErrors = () => {
+  return {
+  type: CLEAR_ERRORS
   }
 }
