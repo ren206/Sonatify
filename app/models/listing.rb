0baf0da032212e7410f6a2c44a58986fdf980917
Michael Ren <michael.ren73@gmail.com>
2017-02-17 10:46:52 -0500

    Add model level validation for ord

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,10 +7,11 @@
 #  song_id     :integer          not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
+#  ord         :integer          not null
 #
 
 class Listing < ActiveRecord::Base
-  validates :playlist_id, :song_id, presence: true
+  validates :playlist_id, :song_id, :ord, presence: true
 
   belongs_to :playlist
   belongs_to :song
