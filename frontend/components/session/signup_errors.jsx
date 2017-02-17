13a8fc8a41a9d634f02c5157eb896ab1277e8e80
Michael Ren <michael.ren73@gmail.com>
2017-02-17 04:59:47 -0500

    Update styling for new error rendering

--- frontend/components/session/signup_errors.jsx
+++ frontend/components/session/signup_errors.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-
-export default props => {
-  const errors = props.errors;
-
-  const findError = field => {
-    let someError = errors.find( error => (error.toLowerCase().includes(field)) );
-    return Boolean(someError) ? someError : "";
-  }
-  const usernameError = findError("username") || "something";
-  const passwordError = findError("password") || "something";
-  const fnameError = findError("f name");
-  const firstNameError = Boolean(fnameError) ? "First" + fnameError.slice(1) : "something";
-  const lnameError = findError("l name");
-  const lastNameError = Boolean(lnameError) ? "Last" + lnameError.slice(1) : "something";
-
-  const usernameClass = findError("username") ? "error" : "";
-  const passwordClass = findError("password") ? "error" : "";
-  const fnameClass = findError("f name") ? "error" : "";
-  const lnameClass = findError("l name") ? "error" : "";
-
-  return (
-    <ul className="signup-errors">
-
-      <li className={ usernameClass }>
-        <span className="error-message">{ usernameError }</span>
-      </li>
-
-      <li className={ passwordClass }>
-        <span className="error-message">{ passwordError }</span>
-      </li>
-
-      <li className={ fnameClass }>
-        <span className="error-message">{ firstNameError }</span>
-      </li>
-
-      <li className={ lnameClass }>
-        <span className="error-message">{ lastNameError }</span>
-      </li>
-
-    </ul>
-  );
-
-};
+// import React from 'react';
+//
+// export default props => {
+//   const errors = props.errors;
+//
+//   const findError = field => {
+//     let someError = errors.find( error => (error.toLowerCase().includes(field)) );
+//     return Boolean(someError) ? someError : "";
+//   }
+//   const usernameError = findError("username") || "something";
+//   const passwordError = findError("password") || "something";
+//   const fnameError = findError("f name");
+//   const firstNameError = Boolean(fnameError) ? "First" + fnameError.slice(1) : "something";
+//   const lnameError = findError("l name");
+//   const lastNameError = Boolean(lnameError) ? "Last" + lnameError.slice(1) : "something";
+//
+//   const usernameClass = findError("username") ? "error" : "";
+//   const passwordClass = findError("password") ? "error" : "";
+//   const fnameClass = findError("f name") ? "error" : "";
+//   const lnameClass = findError("l name") ? "error" : "";
+//
+//   return (
+//     <ul className="signup-errors">
+//
+//       <li className={ usernameClass }>
+//         <span className="error-message">{ usernameError }</span>
+//       </li>
+//
+//       <li className={ passwordClass }>
+//         <span className="error-message">{ passwordError }</span>
+//       </li>
+//
+//       <li className={ fnameClass }>
+//         <span className="error-message">{ firstNameError }</span>
+//       </li>
+//
+//       <li className={ lnameClass }>
+//         <span className="error-message">{ lastNameError }</span>
+//       </li>
+//
+//     </ul>
+//   );
+//
+// };
--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router';
 import { signup } from '../../actions/session_actions';
-import SignupErrors from './signup_errors';
+// import SignupErrors from './signup_errors';
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -25,59 +25,91 @@ class SignupForm extends React.Component {
   }
 
   updateInfo(field) {
-		return event => this.setState({
-			[field]: event.currentTarget.value
-		});
+		return event => {
+      event.preventDefault();
+      this.setState({ [field]: event.currentTarget.value})
+    }
 	}
 
+  // toggleError(field) {
+  //   return event => {
+  //     event.preventDefault();
+  //     this.setState({ [field]: false })
+  //   }
+  // }
+
   render() {
+    const {session} = this.props;
+    const findError = field => {
+      let someError = session.errors.find( error => (error.toLowerCase().includes(field)) );
+      return Boolean(someError) ? someError : "";
+    }
+    const usernameError = findError("username") || "";
+    const passwordError = findError("password") || "";
+    const fnameError = findError("f name");
+    const firstNameError = Boolean(fnameError) ? "First" + fnameError.slice(1) : "";
+    const lnameError = findError("l name");
+    const lastNameError = Boolean(lnameError) ? "Last" + lnameError.slice(1) : "";
+    const usernameClass = findError("username") ? "has-error" : "";
+    const passwordClass = findError("password") ? "has-error" : "";
+    const fnameClass = findError("f name") ? "has-error" : "";
+    const lnameClass = findError("l name") ? "has-error" : "";
 		return (
 			<section className="signup-form">
         <div className="small-signup-logo" />
         <h3>Create your free Sonatify account</h3>
         <br />
 				<form onSubmit={this.handleSubmit}>
-          <SignupErrors
-            errors={ this.props.session.errors }
-            />
 
-          <label htmlFor="username">Username</label>
-            <br/>
-						<input type="text"
-              id="username"
-							value={this.state.username}
-              placeholder="e.g. johndoe"
-							onChange={this.updateInfo("username")} />
-
-					<br/>
-
-					<label htmlFor="password">Password</label>
-            <br/>
-						<input type="password"
-              id="password"
-							value={this.state.password}
-              placeholder="Choose a password"
-							onChange={this.updateInfo("password")} />
+          <p id="wrap-username" className={ usernameClass }>
+            <label htmlFor="username">Username</label>
+              <br/>
+  						<input type="text"
+                id="username"
+  							value={this.state.username}
+                placeholder="e.g. johndoe"
+  							onChange={this.updateInfo("username")} />
+              <div className={ usernameClass }>{usernameError}</div>
+          </p>
 
 					<br/>
 
-					<label htmlFor="fname">First Name</label>
+          <p id="wrap-password" className={ passwordClass }>
+  					<label htmlFor="password">Password</label>
+              <br/>
+  						<input type="password"
+                id="password"
+  							value={this.state.password}
+                placeholder="Choose a password"
+  							onChange={this.updateInfo("password")} />
+              <div className={ passwordClass }>{passwordError}</div>
+          </p>
+
+					<br/>
+
+          <p id="wrap-fname" className={ fnameClass }>
+  					<label htmlFor="fname">First Name</label>
               <br/>
 							<input type="text"
                 id="fname"
 								value={this.state.f_name}
                 placeholder="e.g. John"
 								onChange={this.updateInfo("f_name")} />
+              <div className={ fnameClass }>{firstNameError}</div>
+          </p>
 
 					<br/>
 
-					<label htmlFor="lname">Last Name</label>
-            <br/>
-						<input type="text"
-              id="lname"
-							value={this.state.l_name}
-              placeholder="e.g. Doe"
-							onChange={this.updateInfo("l_name")} />
+          <p id="wrap-lname" className={ lnameClass }>
+  					<label htmlFor="lname">Last Name</label>
+              <br/>
+  						<input type="text"
+                id="lname"
+  							value={this.state.l_name}
+                placeholder="e.g. Doe"
+  							onChange={this.updateInfo("l_name")} />
+              <div className={ lnameClass }>{lastNameError}</div>
+          </p>
 
 					<br/>
 
