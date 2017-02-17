fb719266c1934a9b0c979e66a2adf29ee7d59869
Michael Ren <michael.ren73@gmail.com>
2017-02-17 13:42:29 -0500

    Add playlist routes

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,2 +1,30 @@
 class Api::PlaylistsController < ApplicationController
+  def create
+    @playlist = Playlist.create!(playlist_params)
+    render :show
+  end
+
+  def index
+    @playlists = Playlist.find_by_owner_id(current_user.id)
+    render :index
+  end
+
+  def show
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def update
+    @playlist = Playlist.find(params[:id])
+    @playlist.update!(playlist_params)
+    render :show
+  end
+
+  private
+
+  def playlist_params
+    params.require(:playlist).permit(
+      :name,
+      :owner_id
+    )
+  end
 end
