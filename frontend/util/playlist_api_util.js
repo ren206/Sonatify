b8c047d8727bf2be03cd94f3ca69d62e2fca667f
Michael Ren <michael.ren73@gmail.com>
2017-02-19 13:41:54 -0500

    Create AJAX requests for playlist CRUD

---
+++ frontend/util/playlist_api_util.js
@@ -0,0 +1,24 @@
+export const fetchPlaylists = (playlist) => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/playlists',
+    playlist
+  });
+};
+
+export const fetchPlaylist = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/playlists/${id}`
+  });
+};
+
+
+
+export const createPlaylist = (playlist) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/playlists',
+    playlist
+  });
+};
