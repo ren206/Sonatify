4a238dd58af7e62f45c2db3ab147110427a9dbac
Michael Ren <michael.ren73@gmail.com>
2017-02-19 16:43:36 -0500

    Fetch playlists through user

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,7 @@ class User < ActiveRecord::Base
   attr_reader :password
 
   # user music associations
-  has_many :playlists
-  has_many :songs, through: :playlists
+  has_many :playlists, dependent: :delete_all
 
   # user followings associations
   has_many :in_follows, class_name: "Follow", foreign_key: "followee_id"
