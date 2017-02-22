c09b0689679ae326c929dcd1cf18bca6da682310
Michael Ren <michael.ren73@gmail.com>
2017-02-22 14:34:03 -0500

    Add reference to audio tag

--- frontend/components/web_player/now_playing.jsx
+++ frontend/components/web_player/now_playing.jsx
@@ -7,11 +7,21 @@ class NowPlaying extends React.Component {
   }
 
   render() {
+    // src = { song.audio }
     return(
       <div className="now-playing-wrapper">
         <div id="now-playing">
           <ul>
-            <h3>Now Playing</h3>
+            <li className="now-playing-header">
+              <h3>Now Playing</h3>
+            </li>
+
+            <li className="audio-player">
+              <audio
+                ref={ ref => this.audio = ref }
+                />
+            </li>
+
           </ul>
         </div>
       </div>
