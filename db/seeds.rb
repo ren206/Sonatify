cb8be1fa64d00b51a9e4ded8e9de5edf8b03ffab
Michael Ren <michael.ren73@gmail.com>
2017-02-24 13:50:13 -0500

    Seed using new method

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,52 +1,37 @@
-User.find_or_create_by(
-  username: "guest",
-  password_digest: "$2a$10$ivFZmFpmMRjijSFpeo.QCO1enzsfNJWZp/9PsMXZrG17OfI5ChVRW",
-  f_name: "Guest",
-  l_name: "User"
-)
-User.find_or_create_by(
-  username: "michael",
-  password_digest: "$2a$10$LzMd2UIgX/NmNILhp1SNHO2qZ5qbeLqYNZseXyHmUy3CME5XSZxhy",
-  f_name: "Michael",
-  l_name: "Ren"
-)
-Playlist.destroy_all
-Playlist.find_or_create_by(
-  name: 'Recommended for You',
-  user_id: User.find_by_username("michael").id
-)
+User.find_or_create_by(username: "guest") do |user|
+  user.password_digest = "$2a$10$ivFZmFpmMRjijSFpeo.QCO1enzsfNJWZp/9PsMXZrG17OfI5ChVRW"
+  user.f_name = "Guest"
+  user.l_name = "User"
+end
 
-Artist.destroy_all
+User.find_or_create_by(username: "michael") do |user|
+  user.password_digest = "$2a$10$LzMd2UIgX/NmNILhp1SNHO2qZ5qbeLqYNZseXyHmUy3CME5XSZxhy"
+  user.f_name = "Michael"
+  user.l_name = "Ren"
+end
 
-Artist.create(
+Playlist.find_or_create_by(name: 'Recommended for You') do |playlist|
+  playlist.user_id = User.find_by_username("michael").id
+end
+
+Artist.find_or_create_by(
   name: "John Sayles"
 )
-Artist.create(
-  name: "Morgan Greig"
-)
-Artist.create(
+Artist.find_or_create_by(
   name: "Angelwing"
 )
 
-Album.destroy_all
+Album.destroy_all # TODO: remove this line
 
-Album.create(
-  name: "Classical Songs",
-  artist_id: Artist.find_by_name("John Sayles").id,
-  year: 1700
-)
+Album.find_or_create_by(name: "Classical Songs") do |album|
+  album.artist_id = Artist.find_by_name("John Sayles").id
+  album.year = 1700
+end
 
-Album.create(
-  name: "Blues Songs",
-  artist_id: Artist.find_by_name("Morgan Greig").id,
-  year: 1950
-)
-
-Album.create(
-  name: "Rock Instrumental Music",
-  artist_id: Artist.find_by_name("Angelwing").id,
-  year: 1970
-)
+Album.find_or_create_by(name: "Soothing Instrumentals") do |album|
+  album.artist_id = Artist.find_by_name("Angelwing").id
+  album.year = 1970
+end
 
 Song.destroy_all
 
@@ -62,21 +47,10 @@ Song.create!(
   ord: 2,
   audio: 'https://s3.amazonaws.com/aa-sonatify-dev/seeds/Courante_1st_Cello_Suite_JS_Bach-Jon-Sayles.mp3'
 )
-# Song.create!(
-#   title: "Tammy Morgan",
-#   album_id: Album.find_by_name("Blues Songs").id,
-#   ord: 1,
-#   audio: File.open('app/assets/songs/Tammy-Morgan+Greig.mp3')
-# )
-# Song.create!(
-#   title: "What's Left",
-#   album_id: Album.find_by_name("Blues Songs").id,
-#   ord: 2,
-#   audio: File.open('app/assets/songs/WhatsLeft-Morgan+Greig.mp3')
-# )
+
 Song.create!(
   title: "The Calling",
-  album_id: Album.find_by_name("Rock Instrumental Music").id,
+  album_id: Album.find_by_name("Soothing Instrumentals").id,
   ord: 1,
   audio: 'https://s3.amazonaws.com/aa-sonatify-dev/seeds/theCalling-Angelwing.mp3'
 )
@@ -91,14 +65,6 @@ Listing.create!(
   playlist_id: Playlist.find_by_name("Recommended for You").id,
   song_id: Song.find_by_title("Courante 1st Cello Suite by JS Bach").id
 )
-# Listing.create(
-#   playlist_id: Playlist.find_by_name("Recommended for You").id,
-#   song_id: Song.find_by_title("Tammy Morgan")
-# )
-# Listing.create(
-#   playlist_id: Playlist.find_by_name("Recommended for You").id,
-#   song_id: Song.find_by_title("What's Left")
-# )
 Listing.create!(
   playlist_id: Playlist.find_by_name("Recommended for You").id,
   song_id: Song.find_by_title("The Calling").id
