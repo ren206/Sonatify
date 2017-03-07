b16bf1767a3fc6c84fb0c799d8e13c07327853e3
Michael Ren <michael.ren73@gmail.com>
2017-03-07 14:42:06 -0500

    Dispatch loading before returning the promise

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,19 +1,23 @@
 import * as APIUtil from '../util/playlist_api_util';
 
 export const RECEIVE_PLAYLIST = "RECEIVE_PLAYLIST";
+export const START_LOADING_PLAYLIST = "START_LOADING_PLAYLIST";
 export const RECEIVE_PLAYLISTS = "RECEIVE_PLAYLISTS";
+export const START_LOADING_PLAYLISTS = "START_LOADING_PLAYLISTS";
 export const RECEIVE_NEW_PLAYLIST = "RECEIVE_NEW_PLAYLIST";
 export const RECEIVE_DELETED_PLAYLIST = "RECEIVE_DELETED_PLAYLIST";
 export const RECEIVE_LISTING = "RECEIVE_LISTING";
 export const RECEIVE_PLAYLIST_WITHOUT_SONG = "RECEIVE_PLAYLIST_WITHOUT_SONG";
 
 export const fetchPlaylist = playlistId => dispatch => {
+  dispatch(startLoadingPlaylist);
   return APIUtil.fetchPlaylist(playlistId).then(
     playlist => dispatch(receivePlaylist(playlist))
   );
 }
 
 export const fetchPlaylists = username => dispatch => {
+  dispatch(startLoadingPlaylists);
   return APIUtil.fetchPlaylists(username).then(
     playlists => dispatch(receivePlaylists(playlists))
   );
@@ -48,6 +52,14 @@ export const removeListing = listingId => dispatch => {
     listing => dispatch(receivePlaylistWithoutSong(listing))
   );
 }
+
+export const startLoadingPlaylist = () => ({
+  type: START_LOADING_PLAYLIST
+});
+
+export const startLoadingPlaylists = () => ({
+  type: START_LOADING_PLAYLISTS
+});
 
 export const receivePlaylist = playlist => ({
   type: RECEIVE_PLAYLIST,
