5c6e4a03a9548c1e80ae1529b699f5f8850aebd4
Michael Ren <michael.ren73@gmail.com>
2017-03-20 13:18:35 -0400

    Remove store from the window

--- frontend/sonatify.jsx
+++ frontend/sonatify.jsx
@@ -4,12 +4,12 @@ import Root from './components/root';
 import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
-  // let store;
+  let store;
   if (window.currentUser) {
     const preloadedState = { session: { currentUser: window.currentUser } };
-    window.store = configureStore(preloadedState);
+    store = configureStore(preloadedState);
   } else {
-    window.store = configureStore();
+    store = configureStore();
   }
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store }/>, root);
